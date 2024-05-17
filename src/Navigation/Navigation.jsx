78a1d6062e516a8e4469b3430618522a5975d7de
guskirb <anguskirby1998@gmail.com>
2024-05-17 17:43:25 +0100

    Style search bar

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -5,9 +5,9 @@ function Navigation() {
   return (
     <div className={styles.navBar}>
       <h1 className={styles.header}>TechStore</h1>
-      <div>
-        <input type="text" className={styles.searchBar}></input>
-        <button className="searchButton">Search</button>
+      <div className={styles.searchBar}>
+        <input type="text"></input>
+        <button className={styles.searchButton}></button>
       </div>
       <button>Shop All</button>
       <button>About</button>
