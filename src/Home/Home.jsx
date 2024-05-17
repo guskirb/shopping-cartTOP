c8d08e59badd9396a0eef2bac22ce67baf545061
guskirb <anguskirby1998@gmail.com>
2024-05-17 18:57:29 +0100

    Style home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,12 +2,23 @@ import styles from "./home.module.css";
 
 function Home() {
   return (
-    <div>
+    <div className={styles.homeContainer}>
       <div className={styles.banner}>
         <div className={styles.bannerText}>
           <h1 className={styles.bannerHeader}>The iPhone</h1>
           <p>New Camera. New Design.</p>
           <button>Buy Now</button>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.navBox}>
+          <h2>Shop Laptops</h2>
+        </div>
+        <div className={styles.navBox}>
+          <h2>Shop Headphones</h2>
+        </div>
+        <div className={styles.navBox}>
+          <h2>Shop Phones</h2>
         </div>
       </div>
     </div>
--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -3,15 +3,33 @@ import styles from "./navigation.module.css";
 
 function Navigation() {
   return (
-    <div className={styles.navBar}>
-      <h1 className={styles.header}>TechStore</h1>
-      <div className={styles.searchBar}>
-        <input type="text"></input>
-        <button className={styles.searchButton}></button>
+    <>
+      <div className={styles.navBar}>
+        <h1 className={styles.header}>TechStore</h1>
+        <div className={styles.searchBar}>
+          <input type="text"></input>
+          <button className={styles.searchButton}></button>
+        </div>
+        <button>Shop All</button>
+        <button>About</button>
       </div>
-      <button>Shop All</button>
-      <button>About</button>
-    </div>
+      <div className={styles.saleBar}>
+        <div>
+          <div className={styles.deliveryIcon}></div>
+          <div>
+            <h2>Free Delivery</h2>
+            <p>On orders over $50</p>
+          </div>
+        </div>
+        <div>
+          <div className={styles.collectIcon}></div>
+          <div>
+            <h2>Free Collection</h2>
+            <p>Order online and collect once it's available</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
