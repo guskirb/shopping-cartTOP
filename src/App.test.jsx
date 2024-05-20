884dfdcaa2462313f20f7b30a2de6c0eb0bb56bf
guskirb <anguskirby1998@gmail.com>
2024-05-20 14:36:52 +0100

    Test store cart

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import Cart from "./Navigation/Cart";
 import Navigation from "./Navigation/Navigation";
@@ -15,5 +16,19 @@ describe("Footer component", () => {
   it("renders footer component", () => {
     render(<Footer />);
     expect(screen.getByText('Contact'));
+  });
+});
+
+describe("Cart component", () => {
+  it("renders empty cart component", () => {
+    render(<BrowserRouter><Cart cart={[]} /></BrowserRouter>);
+    expect(screen.getByText('Your cart is empty.'));
+  });
+});
+
+describe("Cart component", () => {
+  it("renders cart component with product", () => {
+    render(<BrowserRouter><Cart cart={[{item:{title:'phone', id: 1}, amount: 1}]} /></BrowserRouter>);
+    expect(screen.getByText('phone'));
   });
 });
